1c8309c0bfb6917d9ed70589cd66e61abd0c971f
Winsor Jeremiah <158396519+walpuerto@users.noreply.github.com>
2025-04-15 15:00:53 +0800

    Implemented Hypothesis Testing

--- hypothesis-test.py
+++ hypothesis-test.py
@@ -1,3 +1,6 @@
+import fscore
+import precomputed
+
 def parseInput(string):
     try:
         return [float(i) for i in string.split(",")]
@@ -6,7 +9,7 @@ def parseInput(string):
 
 def main():
     # Take User Input as elements of groups (A, B, C, ...)
-    print("Welcome to the F-score Computer!")
+    print("Welcome to the ANOVA Computer!")
     groups = []
     for i in range(0, 4):
         ugroup = []
@@ -18,43 +21,50 @@ def main():
         print()
     print()
 
+    if groups == []:   
+        print("No groups were entered. Exiting...")
+        return
+
     # Take significance level
     α = input("α = ")
+    if α != "0.05" and α != "0.01":
+        α = 0.01
+        print(f"\033[Fα = 0.01")
+    else:
+        α = float(α)
     print()
 
-    # Solve N and k
-    N = 0
-    k = len(groups)
-    n = []
-    x = []
-    x̄ = 0
-    for group in groups:
-        N += len(group)
-        n.append(len(group))
-        x.append(sum(group) / len(group))
-        x̄ += sum(group)
-    x̄ /= N
-    print(f"N = {N}")
-    print(f"k = {k}")
-    print(f"dfn = {k-1}")
-    print(f"dfd = {N-k}")
-    print(f"x̄ = {x̄:.2f}")
+    # Step 1: State the Hypothesis
+    print(f"(1) H0: All groups have the same mean.")
+    print(f"    H1: At least one group has a different mean.")
 
-    # Caculate SSB
-    SSB = 0
-    for i in range(k):
-        SSB += n[i] * ((x[i] - x̄) ** 2)
-    print(f"SSB = {SSB:.2f}")
+    # Step 2: State the significance level
+    print(f"(2) α = {α}")
+    
+    # Step 3: Calculate the test statistic
+    fscoreResults = fscore.fscore(groups)
+    print(f"(3) F = {fscoreResults[0]}")
 
-    SSW = 0
-    for i in range(k):
-        for j in groups[i]:
-            SSW += (j - x[i]) ** 2
-    print(f"SSW = {SSW:.2f}")
+    # Step 4: Find the critical value
+    s2df = f"{fscoreResults[1]},{fscoreResults[2]}"
+    CV = precomputed.getCV(s2df, α)
+    print(f"(4) Fα = {CV}")
+    if fscoreResults[0] < CV: print(f"    F < Fα, fail to reject H0")
+    else: print(f"    F > Fα, reject H0")
+    
+    # Step 5: Conclusion
+    if fscoreResults[0] < CV:
+        print(f"(5) Conclusion: All groups have the same mean.")
+    else:
+        print(f"(5) Conclusion: At least one group has a different mean.")
 
-    F = (SSB / (k - 1)) / (SSW / (N - k))
-    print(f"F = {F:.2f}")
-    print()
+    # Ask for another test
+    again = input("Do you want to do another test? (y/n) ")
+    if again.lower() == "y":
+        print("\033[F\033[K", end="\r")
+        main()
+    else:
+        print("thank you for using this calculator! Goodbye! =D")
 
 if __name__ == "__main__":
     main()
