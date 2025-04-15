6a7e06da3afaada7f91bc650e6bc7cc3d8c55298
Winsor Jeremiah <158396519+walpuerto@users.noreply.github.com>
2025-04-15 14:20:37 +0800

    Removed Command-Line Outputs from fscore.py

--- fscore.py
+++ fscore.py
@@ -1,27 +1,4 @@
-def parseInput(string):
-    try:
-        return [float(i) for i in string.split(",")]
-    except:
-        return []
-
-def main():
-    # Take User Input as elements of groups (A, B, C, ...)
-    print("Welcome to the F-score Computer!")
-    groups = []
-    for i in range(0, 4):
-        ugroup = []
-        while parseInput(ugroup) == []:
-            ugroup = input(f"Group {chr(i + 65)} = ")
-            print("\033[F", end="\r")
-            if ugroup == "": break
-        if ugroup != "": groups.append(parseInput(ugroup))
-        print()
-    print()
-
-    # Take significance level
-    α = input("α = ")
-    print()
-
+def fscore(groups, α):
     # Solve N and k
     N = 0
     k = len(groups)
@@ -34,26 +11,17 @@ def main():
         x.append(sum(group) / len(group))
         x̄ += sum(group)
     x̄ /= N
-    print(f"N = {N}")
-    print(f"k = {k}")
-    print(f"dfn = {k-1}")
-    print(f"dfd = {N-k}")
-    print(f"x̄ = {x̄:.2f}")
 
     # Caculate SSB
     SSB = 0
     for i in range(k):
         SSB += n[i] * ((x[i] - x̄) ** 2)
-    print(f"SSB = {SSB:.2f}")
 
     SSW = 0
     for i in range(k):
         for j in groups[i]:
             SSW += (j - x[i]) ** 2
-    print(f"SSW = {SSW:.2f}")
 
     F = (SSB / (k - 1)) / (SSW / (N - k))
-    print(f"F = {F:.2f}")
-    print()
 
-main()
+    return F, N, k, n, x, x̄, SSB, SSW
